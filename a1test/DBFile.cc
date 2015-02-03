215f3bb214a2881a08e9e98770cf5d3b338473c1
gpradeepufl <gpradeepufl@users.noreply.github.com>
2015-02-02 23:27:51 -0500

    DBFile: Adding initial support

--- a1test/DBFile.cc
+++ a1test/DBFile.cc
@@ -6,17 +6,42 @@
 #include "ComparisonEngine.h"
 #include "DBFile.h"
 #include "Defs.h"
-
-// stub file .. replace it with your own DBFile.cc
+#include <iostream>
+#include <fstream>
+#include <string>
 
 DBFile::DBFile () {
 
 }
 
 int DBFile::Create (char *f_path, fType f_type, void *startup) {
+	string mdata_path(f_path);
+
+	if (f_type != heap) {
+		cout << "ERROR: currently not supporting this file type\n";
+		return 0;
+	}
+
+	file.Open(0, f_path);
+	mdata_path.append(".metadata");
+	ofstream mfile(mdata_path.c_str());
+	if (!mfile.is_open()) {
+		cout << "ERROR: Cannot Open meta_data file EXIT !!\n";
+		return 0;
+	}
+
+	mfile << f_type;
+	mfile.close();
+	return 1;
 }
 
 void DBFile::Load (Schema &f_schema, char *loadpath) {
+	FILE *tableFile = fopen (loadpath, "r");
+	Record temp;
+	off_t curLen;
+
+	while (temp.SuckNextRecord (&f_schema, tableFile) == 1)
+		Add(temp);
 }
 
 int DBFile::Open (char *f_path) {
@@ -26,9 +51,23 @@ void DBFile::MoveFirst () {
 }
 
 int DBFile::Close () {
+        off_t curLen;
+
+	curLen = file.GetLength();
+	file.AddPage(&currPage, curLen+1);
+	currPage.EmptyItOut();
+	return file.Close();
 }
 
 void DBFile::Add (Record &rec) {
+        off_t curLen;
+
+	if (!currPage.Append(&rec)) {
+		curLen = file.GetLength();
+		file.AddPage(&currPage, curLen+1);
+		currPage.EmptyItOut();
+		currPage.Append(&rec);
+	}
 }
 
 int DBFile::GetNext (Record &fetchme) {
