bd61337871642e62780e9c51355860498fd31d9e
gpradeepufl <gpradeepufl@users.noreply.github.com>
2015-04-10 01:42:45 -0400

    Statistics: Add initial support for addrel/addatt

--- source/Statistics.cc
+++ source/Statistics.cc
@@ -1,4 +1,38 @@
 #include "Statistics.h"
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+RelInfo::RelInfo(string S, tcnt T)
+{
+	relName = S;
+	numTuples = T;
+};
+
+RelInfo::RelInfo()
+{
+};
+
+RelInfo::~RelInfo()
+{
+};
+
+void RelInfo::AddAttr(string aName, tcnt count)
+{
+	attrInfo[aName] = count;
+	aNames.push_back(aName);
+}
+
+void RelInfo::Print()
+{
+	cout << "---------------------------------------\n";
+	cout << relName << ": " << numTuples << endl;
+
+	for (int i = 0; i < aNames.size(); i++) {
+		cout << aNames[i] <<": "<<attrInfo[aNames[i]] << endl;
+	}
+	cout << "---------------------------------------\n";
+};
 
 Statistics::Statistics()
 {
@@ -12,10 +46,28 @@ Statistics::~Statistics()
 
 void Statistics::AddRel(char *relName, int numTuples)
 {
+	string rName(relName);
+	RelInfo Ri(rName, (tcnt) numTuples);
+
+	RelMap[rName] = Ri;
+	rNames.push_back(rName);
 }
+
+void Statistics::Print()
+{
+	for (int i = 0 ; i < rNames.size(); i ++) {
+		RelMap[rNames[i]].Print();
+	}
+}
+
 void Statistics::AddAtt(char *relName, char *attName, int numDistincts)
 {
+	string rName(relName);
+	string aName(attName);
+
+	RelMap[rName].AddAttr(aName, (tcnt) numDistincts);
 }
+
 void Statistics::CopyRel(char *oldName, char *newName)
 {
 }
--- source/Statistics.h
+++ source/Statistics.h
@@ -1,10 +1,40 @@
 #ifndef STATISTICS_
 #define STATISTICS_
 #include "ParseTree.h"
+#include <string>
+#include <vector>
+#include <tr1/unordered_map>
 
+using namespace std;
+//typedef std::tr1::unordered_map < string, vector <string> > ;
+typedef std::tr1::unordered_map < string, string > Str_to_Str;
+typedef unsigned long long tcnt;
+typedef std::tr1::unordered_map < string, tcnt > Str_to_ULL;
 
+class RelInfo
+{
+private:
+	tcnt numTuples;
+	string relName;
+	Str_to_ULL attrInfo;
+	vector<string> aNames;
+
+public:
+	RelInfo(string S, tcnt T);
+	RelInfo();
+	~RelInfo();
+	void AddAttr(string aName, tcnt count);
+	void Print();
+};
+
+typedef std::tr1::unordered_map < string, RelInfo > Str_to_Ri;
 class Statistics
 {
+private:
+	Str_to_Ri RelMap;
+	vector<string> rNames;
+	Str_to_Str dupe_to_orig;
+	Str_to_Str orig_to_dupe;
 public:
 	Statistics();
 	Statistics(Statistics &copyMe);	 // Performs deep copy
@@ -12,6 +42,7 @@ public:
 
 
 	void AddRel(char *relName, int numTuples);
+	void Print();
 	void AddAtt(char *relName, char *attName, int numDistincts);
 	void CopyRel(char *oldName, char *newName);
 	
